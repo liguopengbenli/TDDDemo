9f317b5ac6187b12826ccb76528f3eb36e18c4b6
guopeng li <guopeng.li@technicolor.com>
2021-02-22 17:11:29 +0100

    finish room instrumentation test

---
+++ app/src/androidTest/java/com/lig/intermediate/unittesting/persistence/NoteDaoTest.java
@@ -0,0 +1,142 @@
+package com.lig.intermediate.unittesting.persistence;
+
+import android.database.sqlite.SQLiteConstraintException;
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.lifecycle.LiveData;
+
+import com.lig.intermediate.unittesting.model.Note;
+import com.lig.intermediate.unittesting.util.LiveDataTestUtil;
+import com.lig.intermediate.unittesting.util.TestUtil;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class NoteDaoTest extends NoteDatabaseTest {
+
+    public static final String TEST_TITLE = "This is a test title"; // control + D -- copie ligne
+    //public static final String TEST_TITLE = "This is a test title";
+    public static final String TEST_CONTENT = "This is some test content";
+    public static final String TEST_TIMESTAMP = "02-2021";
+
+    @Rule
+    public InstantTaskExecutorRule rule = new InstantTaskExecutorRule(); // necessary config
+
+    /*
+        Insert, Read, Delete
+     */
+    @Test
+    public void insertReadDelete() throws Exception{
+        Note note = new Note(TestUtil.TEST_NOTE_1);
+
+        // insert
+        getNoteDao().insertNote(note).blockingGet();
+
+        //read
+        LiveDataTestUtil<List<Note>> liveDataTestUtil = new LiveDataTestUtil<>();
+        List<Note> insertedNotes = liveDataTestUtil.getValue(getNoteDao().getNotes());
+
+        assertNotNull(insertedNotes);
+        assertEquals(note.getContent(), insertedNotes.get(0).getContent());
+        assertEquals(note.getTimestamp(), insertedNotes.get(0).getTimestamp());
+        assertEquals(note.getTitle(), insertedNotes.get(0).getTitle());
+
+
+        note.setId(insertedNotes.get(0).getId());
+        assertEquals(note, insertedNotes.get(0));
+
+        //delete
+        getNoteDao().deleteNote(note).blockingGet();
+
+        //confirm db is empty
+        insertedNotes = liveDataTestUtil.getValue(getNoteDao().getNotes());
+        assertEquals(0, insertedNotes.size());
+    }
+
+    /*
+        Insert, Read, Update, Read, Delete,
+     */
+    @Test
+    public void insertReadUpdatedReadDelete() throws Exception{
+        Note note = new Note(TestUtil.TEST_NOTE_1);
+
+        // insert
+        getNoteDao().insertNote(note).blockingGet();
+
+        //read
+        LiveDataTestUtil<List<Note>> liveDataTestUtil = new LiveDataTestUtil<>();
+        List<Note> insertedNotes = liveDataTestUtil.getValue(getNoteDao().getNotes());
+        assertNotNull(insertedNotes);
+
+        assertEquals(note.getContent(), insertedNotes.get(0).getContent());
+        assertEquals(note.getTimestamp(), insertedNotes.get(0).getTimestamp());
+        assertEquals(note.getTitle(), insertedNotes.get(0).getTitle());
+
+
+        note.setId(insertedNotes.get(0).getId());
+        assertEquals(note, insertedNotes.get(0));
+
+        // update
+        note.setTitle(TEST_TITLE);
+        note.setContent(TEST_CONTENT);
+        note.setTimestamp(TEST_TIMESTAMP);
+        getNoteDao().updateNote(note).blockingGet();
+
+        // read
+        insertedNotes = liveDataTestUtil.getValue(getNoteDao().getNotes());
+        assertEquals(TEST_CONTENT, insertedNotes.get(0).getContent());
+        assertEquals(TEST_TIMESTAMP, insertedNotes.get(0).getTimestamp());
+        assertEquals(TEST_TITLE, insertedNotes.get(0).getTitle());
+        note.setId(insertedNotes.get(0).getId());
+        assertEquals(note, insertedNotes.get(0));
+
+        //delete
+        getNoteDao().deleteNote(note).blockingGet();
+
+        //confirm db is empty
+        insertedNotes = liveDataTestUtil.getValue(getNoteDao().getNotes());
+        assertEquals(0, insertedNotes.size());
+    }
+
+
+    /*
+        Insert note with null title, throw exception
+     */
+
+    @Test(expected = SQLiteConstraintException.class)
+    public void insert_nullTitle_throwSQliteConstraintException() throws Exception{
+        final Note note = new Note(TestUtil.TEST_NOTE_1);
+        note.setTitle(null);
+        //insert
+        getNoteDao().insertNote(note).blockingGet();
+    }
+
+
+    /*
+        Insert, Update with null title, throw exception
+     */
+    @Test(expected = SQLiteConstraintException.class)
+    public void updateNote_nullTitle_throwSQliteConstraintException() throws Exception{
+        Note note = new Note(TestUtil.TEST_NOTE_1);
+
+        // insert
+        getNoteDao().insertNote(note).blockingGet();
+
+        //read
+        LiveDataTestUtil<List<Note>> liveDataTestUtil = new LiveDataTestUtil<>();
+        List<Note> insertedNotes = liveDataTestUtil.getValue(getNoteDao().getNotes());
+        assertNotNull(insertedNotes);
+
+        //update
+        note = new Note(insertedNotes.get(0));
+        note.setTitle(null);
+        getNoteDao().updateNote(note).blockingGet();
+
+    }
+
+
+}
--- app/src/test-common/java/com.lig.intermediate.unittesting/util/TestUtil.java
+++ app/src/test-common/java/com.lig.intermediate.unittesting/util/TestUtil.java
@@ -2,11 +2,26 @@ package com.lig.intermediate.unittesting.util;
 
 import com.lig.intermediate.unittesting.model.Note;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /*
 * It's better to create this package and class for testing
 *
 * */
 
 public class TestUtil {
-    public static Note note = new Note();
+    public static final String TIMESTAMP_1 = "05-2019";
+    public static final Note TEST_NOTE_1 = new Note("Take out the trash", "It's garbage day tomorrow.", TIMESTAMP_1);
+
+    public static final String TIMESTAMP_2 = "06-2019";
+    public static final Note TEST_NOTE_2 = new Note("Anniversary gift", "Buy an anniversary gift.", TIMESTAMP_2);
+
+    public static final List<Note> TEST_NOTES_LIST = Collections.unmodifiableList(
+            new ArrayList<Note>(){{
+                add(new Note(1, "Take out the trash", "It's garbage day tomorrow.", TIMESTAMP_1));
+                add(new Note(2, "Anniversary gift", "Buy an anniversary gift.", TIMESTAMP_2));
+            }}
+    );
 }
