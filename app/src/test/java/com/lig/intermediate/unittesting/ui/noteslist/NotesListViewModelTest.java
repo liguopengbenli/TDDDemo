e3f720016f1174aea954a3954c78aaadd56d7e74
guopeng li <guopeng.li@technicolor.com>
2021-03-03 11:20:53 +0100

    add noteListViewModel test

---
+++ app/src/test/java/com/lig/intermediate/unittesting/ui/noteslist/NotesListViewModelTest.java
@@ -0,0 +1,135 @@
+package com.lig.intermediate.unittesting.ui.noteslist;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.lig.intermediate.unittesting.model.Note;
+import com.lig.intermediate.unittesting.repository.NoteRepository;
+import com.lig.intermediate.unittesting.ui.Resource;
+import com.lig.intermediate.unittesting.util.LiveDataTestUtil;
+import com.lig.intermediate.unittesting.util.TestUtil;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.lig.intermediate.unittesting.repository.NoteRepository.DELETE_FAILURE;
+import static com.lig.intermediate.unittesting.repository.NoteRepository.DELETE_SUCCESS;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+class NotesListViewModelTest {
+    private NotesListViewModel viewModel;
+
+    @Mock
+    /*
+    * mock it means it will not created this class only it will be use as fake class to manipulate
+    * */
+    private NoteRepository noteRepository;
+
+    @BeforeEach
+    public void init(){
+        MockitoAnnotations.initMocks(this);
+        viewModel = new NotesListViewModel(noteRepository);
+    }
+
+
+       /*
+        Retrieve list of notes
+        observe list
+        return list
+     */
+    @Test
+    void retrieveNotes_returnNotesList() throws Exception {
+        // Arrange
+        List<Note> returnedData = TestUtil.TEST_NOTES_LIST;
+        LiveDataTestUtil<List<Note>> liveDataTestUtil = new LiveDataTestUtil<>();
+        MutableLiveData<List<Note>> returnedValue = new MutableLiveData<>();
+        returnedValue.setValue(returnedData);
+        when(noteRepository.getNotes()).thenReturn(returnedValue);
+
+        // Act
+        viewModel.getNotes();
+        List<Note> observedData = liveDataTestUtil.getValue(viewModel.observeNotes());
+
+        // Assert
+        assertEquals(returnedData, observedData);
+    }
+
+
+    /*
+        retrieve list of notes
+        observe the list
+        return empty list
+     */
+
+    @Test
+    void retrieveNotes_returnEmptyNotesList() throws Exception {
+        // Arrange
+        List<Note> returnedData = new ArrayList<>();
+        LiveDataTestUtil<List<Note>> liveDataTestUtil = new LiveDataTestUtil<>();
+        MutableLiveData<List<Note>> returnedValue = new MutableLiveData<>();
+        returnedValue.setValue(returnedData);
+        when(noteRepository.getNotes()).thenReturn(returnedValue);
+
+        // Act
+        viewModel.getNotes();
+        List<Note> observedData = liveDataTestUtil.getValue(viewModel.observeNotes());
+
+        // Assert
+        assertEquals(returnedData, observedData);
+    }
+
+
+    /*
+        delete note
+        observe Resource.success
+        return Resource.success
+     */
+
+    @Test
+    void deleteNote_observeResourceSuccess() throws Exception {
+        // Arrange
+        Note deletedNote = new Note(TestUtil.TEST_NOTE_1);
+        Resource<Integer> returnedData = Resource.success(1, DELETE_SUCCESS);
+        LiveDataTestUtil<Resource<Integer>> liveDataTestUtil = new LiveDataTestUtil<>();
+        MutableLiveData<Resource<Integer>> returnedValue = new MutableLiveData<>();
+        returnedValue.setValue(returnedData);
+        when(noteRepository.deleteNote(any(Note.class))).thenReturn(returnedValue);
+
+        // Act
+        Resource<Integer> observedValue = liveDataTestUtil.getValue(viewModel.deleteNote(deletedNote));
+
+
+        // Assert
+        assertEquals(returnedData, observedValue);
+    }
+
+    /*
+        delete note
+        observe Resource.error
+        return Resource.error
+     */
+    @Test
+    void deleteNote_observeResourceError() throws Exception {
+        // Arrange
+        Note deletedNote = new Note(TestUtil.TEST_NOTE_1);
+        Resource<Integer> returnedData = Resource.error(null, DELETE_FAILURE);
+        LiveDataTestUtil<Resource<Integer>> liveDataTestUtil = new LiveDataTestUtil<>();
+        MutableLiveData<Resource<Integer>> returnedValue = new MutableLiveData<>();
+        returnedValue.setValue(returnedData);
+        when(noteRepository.deleteNote(any(Note.class))).thenReturn(returnedValue);
+
+        // Act
+        Resource<Integer> observedValue = liveDataTestUtil.getValue(viewModel.deleteNote(deletedNote));
+
+
+        // Assert
+        assertEquals(returnedData, observedValue);
+    }
+
+}
